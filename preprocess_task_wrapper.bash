9bcca7de1d773de95b490f24758cda7225407715
LNCD <lncd@Schwarzenagger>
2012-11-19 16:52:47 -0500

    log and fork preprocessing

--- preprocess_task_wrapper.bash
+++ preprocess_task_wrapper.bash
@@ -1,16 +1,28 @@
 #!/bin/bash
 rootdir='/Volumes/TX/Autism_Faces/subject_data/'
 REMOVEALL="YES" 
+MAXJOBS=5
 set -x
 
 script=$(cd $(dirname $0);pwd)/preprocess_task_wf.bash
+logDir=$(cd $(dirname $0);pwd)/logs/
+[ -d $logDir ] || mkdir -p $logDir
 
 cd ${rootdir}/
 
 for subjID in $(ls -d 1* 2*); do
+
+   # if we've put more than $MAXJOBS in the background
+   # sleep for a little bit
+   while [ $(jobs |wc -l) -ge $MAXJOBS ]; do
+    sleep 100
+   done
+
    #for subjID in 1* 2*; do
    export subjID=$subjID  
-   $script || echo "$subjID failed" |  mail -s "error $(date +%F)" aclynn11@gmail.com    
+
+   # () &    means execute all that within and fork to background
+   ( ($script || echo "$subjID failed" |  mail -s "error $(date +%F)" aclynn11@gmail.com) 2>&1 | tee $logDir/$subjID.$(date +%F).log    ) &
 done
 
 echo "finished!" |  mail -s "done $(date +%F)" aclynn11@gmail.com    
